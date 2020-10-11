bfc1e6fc157343f49972f0a5343b86597d840ce7
Oscar Calvo <oscar@calvonet.com>
2020-10-11 15:56:06 -0700

    Update

--- example/rootfs/etc/init.d/set_options.sh
+++ example/rootfs/etc/init.d/set_options.sh
@@ -2,9 +2,13 @@
 
 echo Setting Options
 
+apt-get install -y jq
+
 CONFIG_PATH=/data/options.json
 ACCEPT_EULA=$(jq --raw-output '.ACCEPT_EULA // empty' $CONFIG_PATH)
 export ACCEPT_EULA
 
-./opt/mssql/bin/permission_check.sh
+MSSQL_SA_PASSWORD=$(jq --raw-output '.MSSQL_SA_PASSWORD // empty' $CONFIG_PATH)
+export MSSQL_SA_PASSWORD
+
 
