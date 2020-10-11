8bbecafc5acd56b45c4ee0c9ef3682bcb7f574f8
Oscar Calvo <oscar@calvonet.com>
2020-10-11 14:59:18 -0700

    Set LF

--- example/rootfs/usr/bin/set_options.sh
+++ example/rootfs/usr/bin/set_options.sh
@@ -1,9 +1,10 @@
-echo SettingConfig
+#!/bin/bash
+
+echo Setting Options
 
 CONFIG_PATH=/data/options.json
-ACCEPT_EULA=$(jq --raw-output '.ACCEPT_EULA // empty' $CONFIG_PATH)
+ACCEPT_EULA=$(jq --raw-output '.ACCEPT_EULA // empty' $CONFIGPATH)
 export ACCEPT_EULA
 
-/opt/mssql/bin/permissions_check.sh
-
+./opt/mssql/bin/permissions_check.sh
 
